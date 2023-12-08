a18eb205c325ae924b5ad3fe49604c8432b7fd10
DeviPrasadKL <deviprasadoffcial@gmail.com>
2023-12-08 18:00:41 +0530

    Configured the files

--- src/data/constants.js
+++ src/data/constants.js
@@ -1,7 +1,7 @@
-export const BASE_URI = "https://apf-changemakers-staging.frappe.cloud";
+export const BASE_URI = "http://10.10.0.67";
 
 // must match the "scheme" property of the "app.json" file
-export const REDIRECT_URL_SCHEME = "io.frappe.changemakers"; 
+export const REDIRECT_URL_SCHEME = "dev.buildwithhussain.bwh"; 
 
 export const SECURE_AUTH_STATE_KEY = "AuthState";
-export const OAUTH_CLIENT_ID = "f592ecba60";
+export const OAUTH_CLIENT_ID = "79b99919c7";
